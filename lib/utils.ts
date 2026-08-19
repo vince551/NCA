5e2e728d53997a5722558869ff1aef55ef7ba144
Vince Odhiambo <architectvince7@gmail.com>
2026-08-19 08:31:30 +0300

    Fix Tailwind class merge build error

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,5 +2,5 @@ import { type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(inputs);
+  return twMerge(...inputs);
 }
